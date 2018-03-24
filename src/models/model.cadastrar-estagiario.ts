9c49e3aab8a52b4a526fc75569d965d59e1b790e
thaisdv <thaisdovall494@hotmail.com>
2018-03-24 08:30:39 -0300

    favor não excluir as pasta tudo

---
+++ src/models/model.cadastrar-estagiario.ts
@@ -0,0 +1,6 @@
+export interface Estagiario {
+    nomeEstagiario: string,
+    numeroMatricula: string,
+    telefone: string,
+    email: string
+  }
---
+++ src/models/model.especialidade.ts
@@ -0,0 +1,3 @@
+export interface Especialidade {
+    especialidade: string
+  }
