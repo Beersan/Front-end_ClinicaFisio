d50163deff97548618f87445626237423e8143bd
KeilaStuber <37334419+KeilaStuber@users.noreply.github.com>
2018-03-27 20:15:17 -0300

    model finalizar pre cadastro

---
+++ src/models/model.finalizar-pre-cadastro.ts
@@ -0,0 +1,15 @@
+export interface PreCadastro {
+    nomePaciente: String
+    registroGeral: String,
+    CPF: String,
+    dataNascimento: Date,
+    rendaFamiliar: DoubleRange,
+    endereco: String,
+    numero: String,
+    bairro: String,
+    cidade: String,
+    telefoneUm: String,
+    telefoneDois: String,
+    encaminhamento: String
+    // especialidade: String
+}
