760d0a9a79993533adce3826cf47c7d62c50548a
Julio Dobkovski <32343207+dobkovski@users.noreply.github.com>
2018-04-02 09:13:12 -0300

    Add files via upload

--- src/pages/cadastrar-professor/cadastrar-professor.ts
+++ src/pages/cadastrar-professor/cadastrar-professor.ts
@@ -1,21 +1,77 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CadastrarProfessorPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { NgForm, FormsModule } from '@angular/forms';
+import { AlertController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+//import { ListarProfessorPage } from '../listar-professor/listar-professor';
+import { CadastrarProfessorProvider} from './../../providers/professor/professor';
+import { Professor } from '../../models/model.cadastrar-professor';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
+import { provideModuleLoader } from 'ionic-angular/util/module-loader';
 
 @IonicPage()
 @Component({
   selector: 'page-cadastrar-professor',
   templateUrl: 'cadastrar-professor.html',
 })
-export class CadastrarProfessorPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class CadastrarProfessorPage {
+  //professor: any[];
+  professor: Professor;
+  nomeProfessor: string;
+  matriculaProfessor: string;
+  crefitoProfessor: string;
+  emailProfessor: string;
+  telefoneProfessor: string;
+  especialidadeProfessor: string;
+  idProfessor = "";
+
+
+
+  constructor(
+  	public navCtrl: NavController,
+    public navParams: NavParams,
+    public provider: CadastrarProfessorProvider,
+    private http: HttpClient,
+    public alertCtrl: AlertController
+    ){
+    if (this.navParams.data.professor) {      
+      this.professor = this.navParams.data.professor;
+      console.log(this.professor);
+      var text = JSON.stringify(this.professor);
+      var obj = JSON.parse(text);
+      this.nomeProfessor = obj.nomeProfessor;
+      this.matriculaProfessor = obj.matriculaProfessor;
+      this.crefitoProfessor = obj.crefitoProfessor;
+      this.emailProfessor = obj.emailProfessor;
+      this.telefoneProfessor = obj.telefoneProfessor;
+      this.especialidadeProfessor = obj.especialidadeProfessor;
+  }
+}
+  cadastrarProfessor(){
+  	this.provider.create({
+  		nomeProfessor: this.nomeProfessor,
+  		matriculaProfessor: this.matriculaProfessor,
+  		crefitoProfessor: this.crefitoProfessor,
+  		emailProfessor: this.emailProfessor,
+  		telefoneProfessor: this.telefoneProfessor,
+  		especialidadeProfessor: this.especialidadeProfessor,
+  		idProfessor: this.idProfessor
+  	}).then((result) => {
+      console.log(result);
+      this.showAlert();    
+    });   
+  }
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      subTitle: 'Professor cadastrado.',
+      buttons: ['Ok']
+    });
+    alert.present();
+    this.navCtrl.pop();
   }
 
   ionViewDidLoad() {
