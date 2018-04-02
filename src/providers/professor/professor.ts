2f8494dc28294811d1daaa86115539d44af70d02
Julio Dobkovski <32343207+dobkovski@users.noreply.github.com>
2018-04-02 09:14:41 -0300

    Add files via upload

---
+++ src/providers/professor/professor.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+/*
+  Generated class for the ProfessorProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ProfessorProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ProfessorProvider Provider');
+  }
+
+}
