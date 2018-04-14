5bb76e5a3a5bcee140dbd25c340ef3b4aa985d6c
DANIEL BRESSAN <015613@uniarp.edu.br>
2018-04-14 10:59:35 -0300

    Pronto

--- src/providers/problema/problema.ts
+++ src/providers/problema/problema.ts
@@ -1,27 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, Component, ViewChild } from '@angular/core';
+import { RelatarProblema } from '../../models/model.relatar-problemas';
 import { Http } from '@angular/http';
 import { AlertController } from 'ionic-angular';
 import { Nav, Platform } from 'ionic-angular';
-import { HttpClientModule } from '@angular/common/http'; import { HttpModule } from '@angular/http'
-
-
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 import { HomePage } from '../../pages/home/home';
-import { RelatarProblema } from '../../models/model.relatar-problemas';
 
 @Injectable()
 export class RelatarProblemaProvider {
+  /*
+  assuntoproblema: any;
+  descricaoproblema: any;*/
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ProblemaProvider Provider');
+  push(arg0: any): any {
+    throw new Error("Method not implemented.");
   }
 
-  create(problema: RelatarProblema) {
-    const data = JSON.stringify(problema);
-    console.log(problema);
+  @ViewChild(Nav) nav: Nav;
+  constructor(
+    private http: HttpClient, 
+    public alertCtrl: AlertController
+  ) {}
 
-    this.http.post('http://localhost:3000/problema/relatarProblema', problema).subscribe(response => {
-      console.log(problema);
+  create(problema: RelatarProblema) {
+    return new Promise((resolve, reject) => {
+      this.http.post('http://localhost:3000/problema/relatarProblema', problema).subscribe(response => {
+        resolve(response);
+      });
     });
   }
 }
