e7e85082ddfcb746b89e5a9dffe30456f179a8c5
Julio Dobkovski <32343207+dobkovski@users.noreply.github.com>
2018-04-17 21:39:02 -0300

    Add files via upload

--- src/pages/listar-professores/listar-professores.ts
+++ src/pages/listar-professores/listar-professores.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ListarProfessoresPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { CadastrarProfessorPage } from '../cadastrar-professor/cadastrar-professor';
+import { ProfessorProvider} from './../../providers/professor/professor';
+import { AlertController } from 'ionic-angular';
+import { Professor } from '../../models/model.cadastrar-professor';
 
 @IonicPage()
 @Component({
@@ -14,12 +11,81 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'listar-professores.html',
 })
 export class ListarProfessoresPage {
+  professores: any;
+  especialidades: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private alertCtrl: AlertController, public navCtrl: NavController, public navParams: NavParams, private provider: ProfessorProvider) {
+    this.listarProfessor();
+    this.listarEspecialidade()
   }
-
+  incluir(){
+    this.navCtrl.push(CadastrarProfessorPage, {
+      rootNavCtrl: this.navCtrl
+    });
+  }
   ionViewDidLoad() {
     console.log('ionViewDidLoad ListarProfessoresPage');
   }
 
+  listarProfessor(){
+    this.provider.retornarProfessor().then(
+      data => {
+        this.professores = data;
+        console.log(data);
+      }
+    )
+    .catch(error => alert(error));
+  }
+
+  listarEspecialidade(){
+    this.provider.listar().then(
+      data => {
+        this.especialidades = data;
+        //console.log(data);
+      }
+    )
+    .catch(error => alert(error));
+  }
+
+  excluirProfessor(idprofessor){
+    let alert = this.alertCtrl.create({
+      title: 'Excluir!',
+      message: 'Deseja excluir esse professor?',
+      buttons: [
+        {
+          text: 'Não',
+          role: 'cancel'
+        },
+        {
+          text: 'Excluir',
+          handler: () => {
+            this.provider.excluirProfessor({
+              idProfessor: idprofessor
+            }).then((result) => {
+              this.listarProfessor();
+              this.showAlert();
+            });
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  editar(professor: Professor){
+    this.navCtrl.push(CadastrarProfessorPage, {
+      rootNavCtrl: this.navCtrl,
+      professor: professor
+    });
+  }
+
+
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Sucesso!',
+      subTitle: 'Professor excluído.',
+      buttons: ['Ok']
+    });
+    alert.present();
+  }
 }
